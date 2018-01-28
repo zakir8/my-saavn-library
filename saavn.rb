10a58838f89c3d5c119f29ba540907ea033f6990
Zakir <zakir8@gmail.com>
2018-01-27 19:06:05 -0600

    Essential calls for getting the library are done. Will need to work on the user interaction and outputting the csv file with meta data

--- saavn.rb
+++ saavn.rb
@@ -1,6 +1,6 @@
 =begin
  * This class has all the version 4 Saavn mobile API calls that can be used in many different ways
- * Saavn API always returns status code 200 even if the response has an error message, which is inconvinient
+ * Saavn API always returns status code 200 even if the response has an error message, which is inconvienient
 =end
 require 'net/http'
 require 'json'
@@ -19,29 +19,46 @@ class Saavn
         uri = URI(base_url)
         uri.query = URI.encode_www_form(params)
         response = Net::HTTP.post_form(uri, "username" => user_name, "password" => password)
-        if response.code == "200"
-            #removes annoting empty spaces from the response
-            @cookie = response["set-cookie"]
+        parsed_body = JSON.parse(response.body)
+
+        if parsed_body.dig("data", "email")
+            #removes annoying empty spaces from the response
+            @cookie = response["set-cookie"].split(", ").detect do |cookie|
+                if cookie.include?("I=")
+                    cookie
+                end
+            end
         else
-            raise "Couldn't get your library information. Make sure you entered your information correctly"
+            raise ArgumentError.new("Couldn't get your library information. Make sure you entered your login information correctly")
         end
     end
 
     # this function needs a cookie in order to get user's library
     def get_library
+        raise "No cookie. Please call login first" if self.instance_variable.get(:@cookie).nil?
+
         params = {
             :__call => "library.getAll",
             :_format => "json",
             :_marker => "false",
-            :api_version => 4
+            :api_version => 4,
+            :ctx => "iphoneapp"
         }
 
         uri = URI(base_url)
         uri.query = URI.encode_www_form(params)
-        response = Net::HTTP.get_response(uri)
+
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        # request
+        request = Net::HTTP::Get.new(uri)
+        request["Cookie"] = @cookie
+        response = http.request(request)
+
         if response.code == "200"
             #removes annoying empty spaces from the response
-            response.body.gsub("\n", "")
+            JSON.parse(response.body.gsub("\n", ""))
         else
             raise "Couldn't get your library information. Make sure you entered your information correctly"
         end
@@ -62,18 +79,18 @@ class Saavn
         response = Net::HTTP.get_response(uri)
         if response.code == "200"
             # removes the annoying empty spaces from the response
-            response.body.gsub("\n", "")
+            JSON.parse(response.body.gsub("\n", ""))
         else
             raise "Couldn't get the song information for id's: #{song_ids.join(", ")}"
         end
     end
 
-    # not sure how this could be used, but I will have it for now
+    # not sure how this could be used, but I will have it for now. the endpoints exists on saavn
     def get_launch_data
         #TODO
     end
 
-    # not sure how this could be used, but I will have it for now
+    # not sure how this could be used, but I will have it for now. the endpoints exists on saavn
     def get_social_data
         #TODO
     end
@@ -86,5 +103,5 @@ class Saavn
         def to_param(hsh)
             hsh.inject("") { |glbl,(k,v)| glbl += "&#{k}=#{v}&" }
         end
-
+        
 end
--- saavn_library.rb
+++ saavn_library.rb
@@ -1,3 +1,26 @@
-class SaavnLibrary < Saavn
+require_relative 'saavn'
+ARGV.each do|info|
+    user = ARGV[0]
+    pass = ARGV[1]
 
+    # should have functionality to
+    # get songs in csv
+    # get playlist details in csv
+    # get user details
+
+    s = Saavn.new
+    s.login(user, pass)
+    library = s.get_library
+
+
+    # songs details should have
+    # Name
+    # Singers
+    # Music composers
+    # Lyrics composers
+    # Music Label
+    # 
+    library["song"].each_slice(200) do |song_slice|
+        songs_meta = s.get_songs(song_slice)
+    end
 end
